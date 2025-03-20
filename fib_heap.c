03e03e081f972aa7e4c40d8a4a417b3b1b913374
krarpit <kumar.arpit77@yahoo.com>
2025-03-20 02:19:32 -0700

    Implemented fib heap init functions - now lets write some tests

--- fib_heap.c
+++ fib_heap.c
@@ -1,10 +1,33 @@
+#include <stdlib.h>
+#include <assert.h>
 #include "fib_heap.h"
+
+/**
+ * @brief Initializes a new fibonacci heap
+ */
+fib_heap *fib_heap_create() {
+    fib_heap *new_heap = malloc(sizeof(fib_heap));
+    assert(new_heap != NULL);
+    new_heap->min = NULL;
+
+    ntree_node_list root_list = new_heap->root_list;
+    root_list.items = NULL;
+    root_list.count = 0;
+    root_list.capacity = 0;
+    
+    return new_heap;
+}
 
 /**
 * @brief Inserts an element into the heap
 */
 void fib_heap_insert(fib_heap *fheap, int n) {
-    return; // TODO: stub
+    ntree_node *new_node = ntree_create_node(n);
+    da_append(fheap->root_list, new_node);
+    
+    if (fheap->min == NULL || n <= fheap->min->data) {
+        fheap->min = new_node;
+    }
 }
 
 /**
--- fib_heap.h
+++ fib_heap.h
@@ -13,33 +13,36 @@
  */
 
 typedef struct {
-    ntree_node *items;
+    ntree_node **items;
     size_t count;
     size_t capacity;
-} root_list;
+} ntree_node_list;
 
 #define da_append(list, node)                                                      \
     do                                                                             \
     {                                                                              \
         if (list.count >= list.capacity)                                           \
         {                                                                          \
-            if (list.capacity == 0)                                                \
+            if (list.capacity == 0) {                                              \
                 list.capacity = ROOT_LIST_INIT_CAPACITY;                           \
-            else                                                                   \
+            } else {                                                               \
                 list.capacity *= 2;                                                \
+            }                                                                      \
             list.items = realloc(list.items, list.capacity * sizeof(*list.items)); \
         }                                                                          \
+        list.items[list.count++] = node;                                           \
     } while (0)
 
 /**
- * @brief Represents the fibnoacci heap structure. We store a pointer to the current minimum 
+ * @brief Represents the fibnoacci heap structure. We store a pointer to the current minimum
  * root node, as well as a the root list.
  */
 typedef struct {
     ntree_node *min;
-    root_list root_list;
+    ntree_node_list root_list;
 } fib_heap;
 
-void fib_heap_insert(fib_heap*, int);
-int fib_heap_peek(fib_heap*);
-int fib_heap_pop(fib_heap*);
+fib_heap *fib_heap_create();
+void fib_heap_insert(fib_heap *, int);
+int fib_heap_peek(fib_heap *);
+int fib_heap_pop(fib_heap *);
