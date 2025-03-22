3abcd3ef7d6057ee6604ca7df8588e8325f3e060
krarpit <kumar.arpit77@yahoo.com>
2025-03-21 21:24:59 -0700

    fix darray tests

--- tests/darray_spec.c
+++ tests/darray_spec.c
@@ -10,30 +10,41 @@ Test(DynamicArray, create) {
     cr_assert_eq(0, new_arr->capacity);
 }
 
-// TODO: use a pointer data type, not an int
-//
-// Test(DynamicArray, append) {
-//     darray *arr = da_create();
-//
-//     for (int i = 0; i < 3; i++) {
-//         da_append(arr, i);
-//     }
-//
-//     for (int i = 0; i < 3; i++) {
-//         cr_assert_eq(i, da_get(arr, i));
-//     }
-// }
-//
-// Test(DynamicArray, for_each) {
-//     darray *arr = da_create();
-//
-//     for (int i = 0; i < 3; i++) {
-//         da_append(arr, i);
-//     }
-//
-//     int i, count = 0;
-//     da_for_each(arr, i) {
-//         cr_assert_eq(count, i);
-//         count++;
-//     }
-// }
+Test(DynamicArray, append) {
+    darray *arr = da_create();
+
+    int values[3];
+    int *ptrs[3];
+
+    for (int i = 0; i < 3; i++) {
+        values[i] = i;
+        ptrs[i] = &values[i];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        da_append(arr, ptrs[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cr_assert_eq(ptrs[i], da_get(arr, i));
+    }
+}
+
+Test(DynamicArray, for_each) {
+    darray *arr = da_create();
+
+    int values[3];
+    int *ptrs[3];
+
+    for (int i = 0; i < 3; i++) {
+        values[i] = i;
+        ptrs[i] = &values[i];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        da_append(arr, ptrs[i]);
+    }
+
+    int *i;
+    da_for_each(arr, i) { cr_assert_eq(values[_i], *i); }
+}
